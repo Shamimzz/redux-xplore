31330d87a0aef5c7f6d1050b083be3d439066bc3
Shamimzz <shamimislam5091@gmail.com>
2022-04-27 21:47:05 +0600

    update

---
+++ index.js
@@ -0,0 +1,86 @@
+import { createStore } from 'redux';
+
+const initial = {
+    value: 0,
+    name: "Shamim",
+    taskInput: '',
+    task: [
+        {
+            id: 1, 
+            title: 'First task'
+        },
+        {
+            id: 2, 
+            title: '2nd task'
+        },
+        {
+            id: 3, 
+            title: 'third task'
+        }
+    ]
+  };
+
+function counterReducer(state = initial, action) {
+  switch (action.type) {
+    case 'INCREMENT_ONE': 
+      return { value: state.value + 1 }
+
+    case 'DECREMENT_ONE':
+      return {
+         ...state,
+         value: state.value - 1
+        }
+        // (action.value) astece dispatch theke dynamic value.
+    case 'INCREMENT':
+      return { 
+          ...state,
+          value: state.value + action.value
+         }
+
+    case 'CHANGE_NAME':
+      return { 
+          ...state,
+          name: action.name
+         }
+
+    case 'CHANGE_TASK':
+      return { 
+          ...state,
+          taskInput: action.payload
+        }
+
+    case 'CREATE_TASK':
+      return { 
+          ...state,
+          task: [ ...state.task, action.payload]
+        }
+
+    default:
+      return state 
+  }
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(counterReducer);
+// console.log( "store", store);
+// console.log( "getState", store.getState());
+
+store.subscribe(()=>console.log(store.getState()))
+// Dispatch er maje akta action dite hobe jar maje type thakbe.
+// store.dispatch({type: 'INCREMENT_ONE'})
+
+// store.dispatch({type: 'DECREMENT_ONE'})
+
+// store.dispatch({type: 'INCREMENT', value: 10})
+
+// store.dispatch({type: 'CHANGE_NAME', name: "Rayhan"})
+
+store.dispatch({type: 'CHANGE_TASK', payload: "Annother task from payload"})
+
+const task = {
+   id: new Date().getUTCMilliseconds(),
+   title: store.getState().taskInput
+}
+
+store.dispatch({type: 'CREATE_TASK', payload: task})
